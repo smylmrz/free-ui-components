1d781f436300a2d6125fa94e798b5f371cfaab12
Ismayil Mirzayev <smylmrz@gmail.com>
2023-02-09 15:02:30 +0400

    added header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+
+import Navlink from "./Navlink.vue";
+import Button from "../Button.vue";
+import Search from "../icons/Search.vue";
+import Logo from "./Logo.vue";
+</script>
+
+<template>
+  <header class="py-10">
+    <div class="max-w-7xl mx-auto">
+      <div class="flex items-center justify-between">
+        <Logo/>
+        <div class="flex items-center gap-8">
+          <Navlink to="/components">Components</Navlink>
+          <Navlink to="/components">Donate</Navlink>
+          <Button class="!p-0">
+            <Search />
+          </Button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/components/Header/Navlink.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <RouterLink class="font-semibold text-gray-900 hover:text-gray-600 text-sm">
+    <slot></slot>
+  </RouterLink>
+</template>
