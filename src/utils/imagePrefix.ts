c7937bd5b1e028c42ebaa683ba2b43665989bdc2
Ismayil Mirzayev <smylmrz@gmail.com>
2023-02-20 21:53:30 +0400

    fixed images not loading

--- src/utils/imagePrefix.ts
+++ src/utils/imagePrefix.ts
@@ -1,3 +1,3 @@
 import { isProd } from "./isProd";
 
-export const imagePrefix = () => isProd ? '/preview-images' : '/public/preview-images/'
+export const imagePrefix = () => isProd ? '/preview-images/' : '/public/preview-images/'
