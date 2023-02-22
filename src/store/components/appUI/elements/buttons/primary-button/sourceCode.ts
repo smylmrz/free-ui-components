f6d31e7864e21552ee49994df77741a402532e3f
Ismayil Mirzayev <smylmrz@gmail.com>
2023-02-22 23:39:59 +0400

    fixed primary/secondary buttons

--- src/store/components/appUI/elements/buttons/primary-button/sourceCode.ts
+++ src/store/components/appUI/elements/buttons/primary-button/sourceCode.ts
@@ -1,5 +1,5 @@
 export const sourceCode = `
-<button class="rounded-md bg-red-100 hover:bg-red-200 text-red-500 px-6 py-3">
+<button class="rounded-md bg-red-500 hover:bg-red-600 text-white px-6 py-3">
   Button text
 </button>
 `
