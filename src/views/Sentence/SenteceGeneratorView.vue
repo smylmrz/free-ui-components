311404356d910e14189ece66a561b643acd8ac5d
Ismayil Mirzayev <smylmrz@gmail.com>
2023-03-02 23:37:13 +0400

    added ability to control the amount of sentences

--- src/views/Sentence/SenteceGeneratorView.vue
+++ src/views/Sentence/SenteceGeneratorView.vue
@@ -7,11 +7,20 @@
       </h1>
     </div>
 
-    <div class="p-4 rounded-md bg-slate-200">
+    <label class="mb-5 gap-2 flex">
+      <input v-model="amount" type="number" min="1" class="text-center max-w-fit w-10 border">
+      <span>
+        Number of sentences:
+      </span>
+    </label>
+
+    <div class="p-4 rounded-md bg-slate-200 mb-5">
       {{ sentence }}
     </div>
 
-    <button @click="generate">Regenerate</button>
+    <button class="bg-gray-900 text-white rounded-md px-4 py-2" @click="generate">
+      Regenerate
+    </button>
   </Container>
 </template>
 
@@ -19,7 +28,7 @@
 import { useSentenceGenerator } from "./useSentenceGenerator";
 import Container from "../../components/Component/Container.vue";
 
-const { sentence, generate } = useSentenceGenerator()
+const { sentence, amount, generate } = useSentenceGenerator()
 
 generate()
 </script>
--- src/views/Sentence/useSentenceGenerator.ts
+++ src/views/Sentence/useSentenceGenerator.ts
@@ -1,8 +1,13 @@
 import { nouns } from "./nouns";
 import { verbs } from "./verbs";
-import { ref } from "vue";
+import { adjectives } from "./adjectives";
+import { adverbs } from "./adverbs";
+import { prepositions } from "./prespositions";
+
+import { ref, watch } from "vue";
 export const useSentenceGenerator = () => {
 
+    const amount = ref(2)
     const sentence = ref('')
 
     const randomWordSelector = (words: string[]) => {
@@ -10,14 +15,20 @@ export const useSentenceGenerator = () => {
     }
 
     const generate = () => {
-        const randomName = randomWordSelector(nouns)
-        const randomVerb = randomWordSelector(verbs)
+        sentence.value = ''
 
-        sentence.value = ` The ${randomName} ${randomVerb}`
+        for(let i = 0; i < amount.value; i++) {
+            sentence.value += `The ${randomWordSelector(adjectives)} ${randomWordSelector(nouns)} ${randomWordSelector(adverbs)} ${randomWordSelector(verbs)} because some ${randomWordSelector(nouns)} ${randomWordSelector(adverbs)} ${randomWordSelector(verbs)} ${randomWordSelector(prepositions)} ${randomWordSelector(adjectives)} ${randomWordSelector(nouns)}. `
+        }
     }
+
+    watch([amount], () => {
+        generate()
+    })
 
     return {
         sentence,
-        generate
+        generate,
+        amount
     }
 }
