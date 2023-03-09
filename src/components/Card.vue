3701e8ac1597359ba97b30ee638a983c4dae24d8
Ismayil Mirzayev <smylmrz@gmail.com>
2023-03-09 20:32:23 +0400

    added cards to home page

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+      class="p-10 rounded-3xl"
+      :class="isDark ? 'bg-gray-900' : 'bg-gray-100'"
+  >
+    <h3
+        class="text-xl leading-normal md:text-2xl lg:text-3xl lg:leading-snug font-bold"
+        :class="isDark && 'text-white'"
+    >
+      <slot name="title" />
+    </h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  isDark?: boolean
+}>(),{
+  isDark: false
+})
+</script>
---
+++ src/views/HomeComponentsView.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { useTabs } from "../store/useTabs";
+import { useGroups } from "../store/useGroups";
+import { ref, watchEffect } from "vue";
+import { Group } from "../models/Group";
+import CTA from "../components/CTA.vue";
+import Tab from "../components/Tab.vue";
+import GroupCard from "../components/GroupCard.vue";
+
+const { tabs, setCurrentTab, currentTab, isCurrentTab } = useTabs()
+const { getGroupsByPackage } = useGroups()
+
+const groups = ref<Group[]>([])
+
+watchEffect(() => {
+  groups.value = getGroupsByPackage(currentTab.value.id).slice(0, 12)
+})
+
+</script>
+
+<template>
+  <div>
+    <div class="relative">
+      <div class=" flex flex-col items-center text-center relative space-y-8 z-10">
+        <h1 class="text-4xl md:text-5xl lg:text-7xl font-extrabold">
+          Build your next idea <br> even faster.
+        </h1>
+        <p class="md:text-lg max-w-lg text-slate-700">
+          Beautifully designed, expertly crafted components and templates, built by the makers of Tailwind CSS. The perfect starting point for your next project.
+        </p>
+        <CTA to="/components" type="primary">
+          Start exploring
+          <span class="hidden ml-1 text-slate-400 sm:inline">→</span>
+        </CTA>
+      </div>
+    </div>
+
+    <div class="py-20">
+      <div class="flex gap-5 border-t-2 border-slate-200">
+        <Tab
+            @click="setCurrentTab(tab)"
+            v-for="tab in tabs" :tab="tab" :key="tab.id"
+            :is-active="isCurrentTab(tab)"
+        />
+      </div>
+    </div>
+
+    <div class="grid grid-cols-2 lg:grid-cols-4 gap-5">
+      <GroupCard v-for="group in groups" :key="group.id" :group="group" />
+    </div>
+
+    <div class="max-w-fit mt-10 mx-auto">
+      <CTA type="primary" :to="`/${currentTab.slug}/${currentTab.id}`">Show all</CTA>
+    </div>
+  </div>
+</template>
